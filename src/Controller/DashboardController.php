28496c19787e85cf39584737015ab86ad2a473ec
Yana Rafikova <yana.rafikova2405@mail.ru>
2023-05-25 12:40:11 +0300

    Add user entity, register and auth

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/api/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DashboardController.php',
+        ]);
+    }
+}
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/api/register', name: 'app_registration', methods: 'POST')]
+    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $em = $doctrine->getManager();
+
+        $decoded = json_decode($request->getContent());
+        $email = $decoded->email;
+        $plaintextPassword = $decoded->password;
+
+        $user = new User();
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $user->setEmail($email);
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json(['message' => 'Registered Successfully']);
+    }
+}
